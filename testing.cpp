7dcc8df213838f27fc60eb7167391625dbb2006b
TUTICOI <gdkhanhnguyen2305@gmail.com>
2022-09-19 21:24:49 +0700

    Added testing.cpp

---
+++ testing.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+bool gayTest(string name)
+{
+    if (name == "DuyAnh")
+        return 1;
+    else
+        return 0;
+}
+
+int main()
+{
+    gayTest("DuyAnh");
+    return 0;
+}
